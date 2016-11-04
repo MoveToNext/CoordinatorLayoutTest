e977e94c57d4bcb538f5116ba1d70800d3f1256f
Liuyang <505169591@qq.com>
2016-11-04 10:23:29 +0800

    add change title color

--- appbarlayout/src/main/java/com/liu/appbarlayout/MainActivity.java
+++ appbarlayout/src/main/java/com/liu/appbarlayout/MainActivity.java
@@ -1,6 +1,8 @@
 package com.liu.appbarlayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        CollapsingToolbarLayout CollapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.CollapsingToolbarLayout);
+        CollapsingToolbarLayout.setTitle("CollapsingToolbar");
+        CollapsingToolbarLayout.setExpandedTitleTextAppearance(R.style.Toolbar_TitleText);
+        //通过CollapsingToolbarLayout修改字体颜色
+        CollapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);//设置还没收缩时状态下字体颜色
+        CollapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);//设置收缩后Toolbar上字体的颜色
     }
 }
